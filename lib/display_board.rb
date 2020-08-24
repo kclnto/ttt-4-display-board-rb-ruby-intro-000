87cc764afb0ed879c448e5bf2f2c384e0adf0b3e
Kacy Trujillo <kclnto@gmail.com>
2020-08-24 19:15:05 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,8 +4,9 @@ board = ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "]
 
 def display_board(board)
   print " #{board[0]} | #{board[1]} | #{board[2]} \n
-          
+          -----------\n
           #{board[3]} | #{board[4]} | #{board[5]} \n
+          -----------\n
           #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
